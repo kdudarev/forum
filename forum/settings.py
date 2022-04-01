96557a2e804244f82093f5769cc68812ce52fc9c
Kirill <k.dudarev96@gmail.com>
2022-04-01 09:08:56 +0300

    Some fixes

--- forum/settings.py
+++ forum/settings.py
@@ -15,9 +15,8 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
-env_path = Path('.')/'.env'
-load_dotenv(dotenv_path=env_path)
+
+load_dotenv(dotenv_path=Path('.')/'.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,7 +89,7 @@ WSGI_APPLICATION = 'forum.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": os.getenv("DB_ENGINE"),
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
